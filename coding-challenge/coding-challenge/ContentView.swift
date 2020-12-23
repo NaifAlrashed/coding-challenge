f71ea8dee700cacaafef64072b54ab033ca0cb3f
naif <naifdev@gmail.com>
2020-12-23 11:21:26 +0300

    create xcode project

---
+++ coding-challenge/coding-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coding-challenge
+//
+//  Created by Naif Alrashed on 23/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
